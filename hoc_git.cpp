71cfd6532fd51d8c9823de5e19a2e5093c5aa899
unknown <kontonfon@gmail.com>
2021-06-28 10:17:51 +0700

    Dành cả ngày để học git cùng mọi người nhé

---
+++ hoc_git.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define dbg(x) cout<<#x<<" : "<<x<<endl
+#define rep(i,a,b) for(int i=(a);i<=(b);i++)
+#define inf 1000000000000000000
+priority_queue<ll,vector<ll>,greater<int>> qp;
+priority_queue<ll> qr;
+#define maxn 100005
+#define mod 1000000007
+#define cnt_ones __builtin_popcount
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    cout<<"GIT COMMIT";
+    return 0;
+}
